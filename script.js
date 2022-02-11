f902ecdb0e602e86267faf088976261243e7f734
Juan Teheran <juante@tirant.com>
2022-02-11 13:35:11 -0500

    yo si se que es un factorial

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const indice = 10
+const indice = 9
 
 let a = 1;
 let b = 1;
@@ -8,5 +8,14 @@ for (let i = 0; i < indice; i++) {
     a = b
     b = c
     c = a + b;
-    console.log(c);
 }
+
+console.log(c);
+
+let factorial = 1
+
+for (let i = 2; i <= indice; i++) {
+    factorial = factorial * i
+}
+
+console.log(factorial);
